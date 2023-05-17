9fcf1610598fa7cb03c81bcb3e77278de47d0024
EllGree <96187296+EllGree@users.noreply.github.com>
2023-05-17 17:01:32 +0200

    OGG to Mpeg convert

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import { Telegraf } from 'telegraf';
+import { message } from 'telegraf/filters'
+import { code } from 'telegraf/format';
 import config from 'config';
-const bot = new Telegraf(config.get('TELEGRAM_BOT'));
-bot.launch();
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
+import { ogg } from './ogg.js'
+
+const bot = new Telegraf(config.get('TELEGRAM_BOT'))
+
+bot.on(message('voice'), async (ctx) => {
+    try {
+        await ctx.reply(code('Сообщение принял. Жду ответ от сервера...'))
+        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+        const userId = String(ctx.message.from.id)
+        const oggPath = await ogg.create(link.href, userId)
+        const mp3Path = await ogg.toMp3(oggPath, userId)
+    } catch (e) {
+        console.log(`Error while voice message`, e.message)
+    }
+})
